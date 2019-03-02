0fa640d671c349b5759c39fa2863d80a59550e13
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-03-02 20:54:14 +0530

    commit the life cycle method

---
+++ build/precache-manifest.fe1165b7135d4015dafe1e4a1bbf5e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "608b6b3b0e2f9c527129",
+    "url": "/static/js/main.608b6b3b.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "608b6b3b0e2f9c527129",
+    "url": "/static/css/main.c2988011.chunk.css"
+  },
+  {
+    "revision": "832ff01bc320ccb19affd9906d2bfa03",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// uglification
+
+// minification -> remove white space
+
+// Two types of components
+
+// 1. Class Component // statefull component
+
+// 2. Function Component // stateless component
+
+// const App = () => (
+//   <Fragment>
+//     <h1>Hello World</h1>
+//     <h1>Hello World</h1>
+//   </Fragment>
+// );
+
+//  App -> click listner -> unmount  -> remoe lister  -> Test -> click
 
 class App extends Component {
+  state = {
+    text: {
+      greet: "hello",
+      isError: false
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    return {
+      greet: `${props.text} greet`
+    };
+  }
+
+  // UNSAFE_componentWillMount() {
+  //   console.log("UNSAFE_componentWillMount");
+  // }
+
+  // UNSAFE_componentWillReceiveProps() {}
+
+  // UNSAFE_componentWillUpdate() {}
+
+  componentDidMount() {
+    document.addEventListener("click", () => {
+      console.log("click");
+    });
+
+    // this.interval = setInterval(() => {
+    //   console.log("hello interval");
+    // }, 1000);
+
+    this.firstH1.setAttribute("class", "h1Class");
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+    return "hello world";
+  }
+
+  componentDidUpdate = (prevProps, prevState, snapshot) => {
+    console.log(snapshot);
+    console.log("componentDidUpdate");
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+    document.removeEventListener("click");
+  };
+
+  static getDerivedStateFromError(error) {
+    return {
+      isError: true
+    };
+  }
+
+  componentDidCatch(error, info) {}
+
   render() {
+    console.log("render");
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload 1.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <h1
+          ref={ref => {
+            this.firstH1 = ref;
+          }}
+        >
+          {this.state.text.greet}
+        </h1>
+        <h1>{this.state.greet}</h1>
+        <button
+          name="hello"
+          onClick={() => {
+            this.setState({
+              text: { ...this.state.text, greet: "hello world" }
+            });
+          }}
+        >
+          Click Me
+        </button>
+      </Fragment>
     );
   }
 }
